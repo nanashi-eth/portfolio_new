deaead76105f6025cdfdd2c04943caf2a20da266
Nanashi <jose98_rbb@hotmail.com>
2024-08-27 23:03:22 +0200

    [UPD] updated some visual features

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import{FaGithub} from 'react-icons/fa'
-import{FiDribbble} from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
